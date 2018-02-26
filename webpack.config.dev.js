53fb97d3e19e587a720a885dc0a5e60b022e64c9
Heaten Mistry <windH800000000@gmail.com>
2018-02-26 17:20:58 -0500

    new/updated files

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -42,7 +42,7 @@ export default {
 			{test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"},
 			{test: /\.css$/, use: [
 				{loader: "style-loader"},
-				{loader: "css-loader", options: {modules: true},}
+				{loader: "css-loader", options: {modules: true}}
 			]}
 		]
 	}
