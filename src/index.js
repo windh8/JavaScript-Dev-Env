c577c3c5ce9c3e343b464ffee92cd40c66d94ced
Aditya Trivedi <adityatr@outlook.com>
2018-10-13 18:58:09 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,4 @@ import numeral from "numeral";
 //numeral library
 const courseValue = numeral(1000).format("$0,0.00");
 
-//The string below uses a ES6 template string feature (from ES6) hence must use
-// ` instead of ' or "
-console.log(`I would pay ${courseValue} for this awesome course!`);
+
